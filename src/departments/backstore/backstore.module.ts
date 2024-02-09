e9a9e6bbe58de39548d9d005f6113f6aaa9eabc8
Richard Mulambuzi <richardmlambuzi5@gmail.com>
2024-02-09 09:13:46 +0200

    new changes
    new changes

--- src/departments/backstore/backstore.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BackstoreController } from './backstore.controller';
-import { BackstoreService } from './backstore.service';
-@Module({
-  
-    providers: [BackstoreService],
-    controllers:[BackstoreController]
-  })
-export class BackstoreModule {}
